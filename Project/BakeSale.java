9034f18acec995a88ee5ab9a6a1a52712271d8cf
Nicholas Nguyen <95522691+nicholasnguyen01@users.noreply.github.com>
2024-07-27 00:19:33 -0700

    Add files via upload

---
+++ Project/BakeSale.java
@@ -0,0 +1,12 @@
+package Project;
+
+public class BakeSale {
+  public static void main(String[] args) {
+    int numCookies = 17;
+    numCookies -= 3;
+    numCookies /= 2;
+
+    // Add your code above
+    System.out.println(numCookies);
+  }
+}
