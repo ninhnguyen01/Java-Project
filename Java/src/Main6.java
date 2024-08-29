daacf18bcf225177944b4459923c055dd2f45a94
Ninh Nguyen <95522691+ninhnguyen01@users.noreply.github.com>
2024-08-29 15:42:49 -0700

    Delete Java/src directory

--- Java/src/Main6.java
+++
@@ -1,9 +0,0 @@
-public class Main6 {
-    public static void main(String[] args) {
-        sayHello();
-    }
-    public static void sayHello() {
-        System.out.println("Hello Alex!");
-    }
-
-}
